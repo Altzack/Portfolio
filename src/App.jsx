8e5bd32d23e617db18a2e4fad3bd7349721143eb
Zack <altschulerzack@yahoo.com>
2025-05-12 19:10:27 -0700

    fix for big screens

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow container mx-auto px-4 py-8">
+      <main className="flex-grow max-w-screen-2xl mx-auto px-4 py-8">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
