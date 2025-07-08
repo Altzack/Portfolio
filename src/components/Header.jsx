c85ba436d2efc40fe9b8e1e43f2cda9711feb984
Zack <altschulerzack@yahoo.com>
2025-07-07 23:17:16 -0700

    icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function Header() {
@@ -6,7 +7,7 @@ export default function Header() {
     <header className="bg-background text-text shadow">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center border-b-1 border-gray-400 border-accent">
         <Link to="/" className="text-2xl font-bold hover:text-gray-400">
-          Zack Altschuler
+          <FaHome className="inline-block mr-2" size={24} />
         </Link>
         <nav className="space-x-4">
           <Link to="/projects" className="text-text hover:text-gray-400">
